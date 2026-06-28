638615cd3ffdaafa0cc9e2bdbfa34c59bb2570b9
Stuart Urback <stuart@urback.net>
2026-06-28 14:49:52 -0600

    Remove mermaid

--- .eleventy.js
+++ .eleventy.js
@@ -61,15 +61,6 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.setLibrary("md", markdownLib);
 
-  // setup mermaid markdown highlighter
-  const highlighter = eleventyConfig.markdownHighlighter;
-  eleventyConfig.addMarkdownHighlighter((str, language) => {
-    if (language === "mermaid") {
-      return `<pre class="mermaid">${str}</pre>`;
-    }
-    return highlighter(str, language);
-  });
-
   eleventyConfig.addPassthroughCopy({ "src/images": "images" });
   eleventyConfig.setServerOptions({ watch: [manifestPath] });
 
